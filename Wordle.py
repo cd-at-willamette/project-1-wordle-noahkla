8c7fac5801fd0127f703c716a1e53ff09c5f1683
noahkla <noahklarreich@gmail.com>
2024-09-23 11:16:57 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -19,18 +19,46 @@ def wordle():
         for i in range(5):
             w += gw.get_square_letter(0, i)
         w = w.lower()
-        print(w)
         if w in ENGLISH_WORDS:
             gw.show_message('Valid word!')
         else:
             gw.show_message('Not a valid word')
+        color_row(0, 'glass')
       
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-    
 
+    def color_row(r, a):
+        a2 = a
+        correct = []
+        a = list(a)
+        for i in range(len(a2)):
+            l = gw.get_square_letter(r, i).lower()
+            if l == a[i]:
+                gw.set_square_color(r, i, CORRECT_COLOR)
+             #   print(l, a[i])
+                correct.append(i)
+                a[i] = '0'
+                a2 = ''
+                for j in a:
+                    a2 += j
+        for i in range(len(a2)):
+            l = gw.get_square_letter(r, i).lower()
+            if i not in correct:
+                if l in a:
+                        gw.set_square_color(r, i, PRESENT_COLOR)
+                        index = a2.rfind(l)
+                        a[index] = '0'
+                        a2 = ''
+                        for j in a:
+                            a2 += j
+                       # print(l, a[i], index)
+                else:
+                    gw.set_square_color(r, i, MISSING_COLOR)
+                  #  print(l, a[i])
+      #  print(a)
 
 # Startup boilerplate
 if __name__ == "__main__":
